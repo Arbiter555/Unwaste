9764a9b0b6ac48be61f220fb1da7ce8afc1fa43a
dlin4 <dlin4@uiowa.edu>
2023-11-05 19:33:40 -0600

    Initial commit

---
+++ app/src/main/java/com/example/unwaste/MapsActivity.kt
@@ -0,0 +1,72 @@
+package com.example.unwaste
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.unwaste.databinding.ActivityMapsBinding
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.model.TypeFilter
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
+import com.google.android.gms.common.api.Status
+import com.google.maps.DirectionsApi
+import com.google.maps.DirectionsApiRequest
+import com.google.maps.GeoApiContext
+import com.google.maps.model.DirectionsResult
+import com.google.maps.model.TravelMode
+
+
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        // Initialize the SDK
+        Places.initialize(applicationContext, "@string/maps_api_key")
+
+        // Create a new PlacesClient instance
+        val placesClient = Places.createClient(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val iowaCity = LatLng(41.66113, -91.53017)
+        mMap.addMarker(MarkerOptions().position(iowaCity).title("Click on me to get places and routes!!!"))
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(15F))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(iowaCity))
+    }
+}
+
+
+
+
---
+++ app/src/main/java/com/example/unwaste/loading.kt
@@ -0,0 +1,40 @@
+package com.example.unwaste
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent // For starting the main activity
+import android.os.Handler // For delaying the transition to the main activity
+import android.widget.ProgressBar
+import android.widget.Button
+import android.os.Looper
+
+
+
+class loading : AppCompatActivity() {
+    //private lateinit var progressBar: ProgressBar
+    private lateinit var startButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+
+        // Initialize views
+        //progressBar = findViewById(R.id.loadingProgressBar)
+        startButton = findViewById(R.id.startButton)
+
+        // Disable the button during loading
+        startButton.isEnabled = false
+
+        // Simulate a loading task with a delay
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Task completed, enable the button
+            startButton.isEnabled = true
+            // Handle button click to start the main activity
+            startButton.setOnClickListener {
+                val intent = Intent(this, MapsActivity::class.java)
+                startActivity(intent)
+                finish() // Close the loading activity
+            }
+        }, 2000) // Adjust the delay as needed
+    }
+}
