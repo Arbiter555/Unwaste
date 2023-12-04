c5a1a4e175d943fc3bede7e0ace873dfe378547f
jacksonmgrant <jacksonmgrant@gmail.com>
2023-12-04 15:18:45 -0600

    Fix activity interchange

--- app/src/main/java/com/example/unwaste/UserLogin.kt
+++ app/src/main/java/com/example/unwaste/UserLogin.kt
@@ -1,30 +1,50 @@
 package com.example.unwaste
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.navigation.Navigation.findNavController
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.example.unwaste.R
-import com.example.unwaste.databinding.ActivityMainBinding
-
-import com.example.unwaste.databinding.FragmentLoginBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
 
 class UserLogin : AppCompatActivity() {
+    private lateinit var startButton: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_login)
 
         // Additional logic for your UserLogin activity
+
+        // Initialize views
+        //progressBar = findViewById(R.id.loadingProgressBar)
+        startButton = findViewById(R.id.button)
+
+        // Disable the button during loading
+        startButton.isEnabled = false
+
+        // Simulate a loading task with a delay
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Task completed, enable the button
+            startButton.isEnabled = true
+            // Handle button click to start the main activity
+            startButton.setOnClickListener {
+                // To test inventory activity
+                // val intent = Intent(this, MainActivity::class.java)
+                // startActivity(intent)
+
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+
+
+                // To test maps activity
+                //val intent = Intent(this, MapsActivity::class.java)
+                //startActivity(intent)
+                finish() // Close the loading activity
+            }
+        }, 300) // Adjust the delay as needed
     }
 }
 
